ab128c0bc44f04555fab1dbffa0286f3b6d4e7b7
luojxxx <luo.j2010@gmail.com>
2018-01-06 21:17:59 -0800

    Setup backend error messages to appear only in development

--- backend/app.js
+++ backend/app.js
@@ -117,8 +117,8 @@ app.use(function(req, res, next) {
 // Error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = (process.env.STAGE === 'development') ? err.message : 'Error';
+  res.locals.error = (process.env.STAGE === 'development') ? err : {};
 
   // render the error page
   res.status(err.status || 500);
