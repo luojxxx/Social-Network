3844d3d2270cd00ee2af816ab7cbc0c5a2cb416e
luojxxx <luo.j2010@gmail.com>
2018-01-26 19:15:57 -0800

    Added dynamically assigned port listener to backend for heroku

--- app.js
+++ app.js
@@ -127,6 +127,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// Set listener since heroku dynamically assign app a port
+app.listen(process.env.PORT || 5000)
+
 module.exports = app;
 
 
