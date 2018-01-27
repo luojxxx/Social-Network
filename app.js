97ba57d71217d839fc1833c5cbe26b0bbb0ff490
luojxxx <luo.j2010@gmail.com>
2018-01-26 19:08:24 -0800

    Removed favicon import

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cors = require('cors');
 var cookieParser = require('cookie-parser');
